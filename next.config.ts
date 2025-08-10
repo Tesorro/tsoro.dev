d3d4fcf3095563e7d20f2c247eb9261f0c24111b
Zaur <z.tsoroev@original-group.ru>
2025-08-10 09:14:32 +0300

    init commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // sassOptions: {
+  //   includePaths: [path.join(__dirname, 'app', 'globals.scss')]
+  // },
+  reactStrictMode: true,
+  poweredByHeader: false,
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/uploads/:path*',
+  //       destination: `${process.env.SERVER_URL}/uploads/:path*`
+  //     }
+  //   ]
+  // }
 };
 
 export default nextConfig;
